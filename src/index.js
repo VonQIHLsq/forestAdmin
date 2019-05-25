bfcc54ab01680191d6f0bfd67b68464b4ed3d230
qh <2q@qhdeMacBook-Pro.local>
2019-05-25 15:53:46 +0800

    项目的基本配置完成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import zhCN from 'antd/lib/locale-provider/zh_CN'
+import { LocaleProvider } from 'antd'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <LocaleProvider locale={zhCN}>
+        <App />
+    </LocaleProvider>,
+    document.querySelector('#root')
+)
